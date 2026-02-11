125945954906328240653e54a53b37dc19c672f6
thekhrafid <mdrafidhossen44@gmail.com>
2026-02-11 23:32:58 +0600

    Fixed: fix prisma error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "../lib/generated/prisma/client";
 
 const connectionString = process.env.DIRECT_URL as string;
 
